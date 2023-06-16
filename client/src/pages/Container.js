c8ca5e2f22bf46c044ec7948b08ba8ebe360083e
unknown <kcavner@hotmail.com>
2023-06-16 13:20:14 -0500

    blacked out

--- client/src/pages/Container.js
+++ client/src/pages/Container.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Plot from 'react-plotly.js';
 
 export default function Container() {
@@ -9,11 +9,13 @@ export default function Container() {
   const [c, setC] = useState([]);
   const [pointValue, setPointValue] = useState(3142);
   const [updateSpeed, setUpdateSpeed] = useState(1);
-  const [paused, setPaused] = useState(false); // New state variable for pause/resume
+  const [paused, setPaused] = useState(false);
+  const plotRef = useRef(null);
+  const [cameraPosition, setCameraPosition] = useState(null);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (!paused) { // Only update if not paused
+      if (!paused) {
         setPointCount(prevPointCount => {
           const newPointCount = prevPointCount + updateSpeed;
           return newPointCount <= 6000 ? newPointCount : 1;
@@ -24,7 +26,7 @@ export default function Container() {
     return () => {
       clearInterval(interval);
     };
-  }, [updateSpeed, paused]); // Include paused state in the dependency array
+  }, [updateSpeed, paused]);
 
   useEffect(() => {
     generateValues();
@@ -38,9 +40,9 @@ export default function Container() {
 
     for (let i = 0; i < pointCount; i++) {
       const r = i + (pointCount - i);
-      newX.push(r * Math.sin(1*i) + Math.cos(pointValue / 1000));
+      newX.push(r * Math.sin(1 * i) + Math.cos(pointValue / 1000));
       newY.push(r * Math.cos(i) + (pointValue / 1000));
-      newZ.push(r * Math.sin(3*i) +Math.cos(pointValue / 1000));
+      newZ.push(r * Math.sin(3 * i) + Math.cos(pointValue / 1000));
       newC.push(Math.sin(i));
     }
 
@@ -65,39 +67,23 @@ export default function Container() {
     setPaused(prevPaused => !prevPaused);
   };
 
+  const handlePlotRelayout = (eventData) => {
+    if (eventData?.["scene.camera"]) {
+      const newCameraPosition = eventData["scene.camera"];
+      setCameraPosition(newCameraPosition);
+    }
+  };
+
+  useEffect(() => {
+    if (plotRef.current && cameraPosition) {
+      plotRef.current.relayout({ 'scene.camera': cameraPosition });
+    }
+  }, [cameraPosition]);
+
   return (
     <div>
-      <div>
-        <label htmlFor="pointValue">Point Value:</label>
-        <input
-          type="range"
-          id="pointValue"
-          min="1"
-          max="6000"
-          value={pointValue}
-          onChange={handlePointValueChange}
-        />
-        <span>{pointValue}</span>
-      </div>
-      <div>
-        <label htmlFor="updateSpeed">Update Speed:</label>
-        <input
-          type="range"
-          id="updateSpeed"
-          min="5"
-          max="100"
-          step="5"
-          value={updateSpeed}
-          onChange={handleUpdateSpeedChange}
-        />
-        <span>{(updateSpeed / 100).toFixed(2)}s</span>
-      </div>
-      <div>
-        <button onClick={handlePauseResume}>
-          {paused ? "Resume" : "Pause"}
-        </button>
-      </div>
-      <Plot  
+      <Plot
+        ref={plotRef}
         data={[
           {
             type: 'scatter3d',
@@ -110,18 +96,49 @@ export default function Container() {
               width: 3,
               color: c,
               colorscale: 'Viridis'
-            },
+            }
           }
         ]}
-        layout={{ 
+        layout={{
           width: 600,
           height: 600,
           title: "Dynamic Plot",
-          scene:{bgcolor:"black"},
-          paper:{bgcolor:'black'}
+          scene: { bgcolor: "black" },
+          paper_bgcolor: "black",
+          font: { color: "white" }
         }}
-       
+        onRelayout={handlePlotRelayout}
       />
+      <div className="plot">
+        <label className="pointValue" htmlFor="pointValue">Point Value:</label>
+        <input
+          type="range"
+          id="pointValue"
+          min="1"
+          max="6000"
+          value={pointValue}
+          onChange={handlePointValueChange}
+        />
+        <span>{pointValue}</span>
+      </div>
+      <div className="plot">
+        <label className="updateSpeed" htmlFor="updateSpeed">Update Speed:</label>
+        <input
+          type="range"
+          id="updateSpeed"
+          min="5"
+          max="100"
+          step="5"
+          value={updateSpeed}
+          onChange={handleUpdateSpeedChange}
+        />
+        <span>{(updateSpeed / 100).toFixed(2)}s</span>
+      </div>
+      <div className="plot">
+        <button onClick={handlePauseResume}>
+          {paused ? "Resume State" : "Pause State"}
+        </button>
+      </div>
     </div>
   );
 }
